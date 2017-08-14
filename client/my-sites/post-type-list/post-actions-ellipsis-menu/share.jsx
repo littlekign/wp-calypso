c4ccdc151700cb644572e388d7a4bef7a869c6cb
Miklos Juhasz <miklos.juhasz@automattic.com>
2017-08-14 23:01:29 +0200

    Add Share action menu item.

---
+++ client/my-sites/post-type-list/post-actions-ellipsis-menu/share.jsx
@@ -0,0 +1,59 @@
+/**
+ * External dependencies
+ */
+import React, { Component, PropTypes } from 'react';
+import { connect } from 'react-redux';
+import { localize } from 'i18n-calypso';
+import { includes } from 'lodash';
+
+/**
+ * Internal dependencies
+ */
+import PopoverMenuItem from 'components/popover/menu-item';
+import { mc } from 'lib/analytics';
+import { getPost } from 'state/posts/selectors';
+import config from 'config';
+
+class PostActionsEllipsisMenuShare extends Component {
+	static propTypes = {
+		globalId: PropTypes.string,
+		translate: PropTypes.func.isRequired,
+		status: PropTypes.string,
+	};
+
+	constructor() {
+		super( ...arguments );
+
+		this.sharePost = this.sharePost.bind( this );
+	}
+
+	sharePost() {
+		mc.bumpStat( 'calypso_cpt_actions', 'share' );
+	}
+
+	render() {
+		const { translate, status } = this.props;
+		if ( ! config.isEnabled( 'posts/post-type-list' ) || ! includes( [ 'publish' ], status ) ) {
+			return null;
+		}
+
+		return (
+			<PopoverMenuItem onClick={ this.sharePost } icon="share">
+				{ translate( 'Share' ) }
+			</PopoverMenuItem>
+		);
+	}
+}
+
+export default connect(
+	( state, ownProps ) => {
+		const post = getPost( state, ownProps.globalId );
+		if ( ! post ) {
+			return {};
+		}
+
+		return {
+			status: post.status,
+		};
+	}
+)( localize( PostActionsEllipsisMenuShare ) );
